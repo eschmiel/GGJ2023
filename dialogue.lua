e0122a0373793f306b53086fc15438514cc68704
eschmiel <eschmiel@gmail.com>
2023-02-09 23:28:13 -0500

    reshape dialogue box

--- dialogue.lua
+++ dialogue.lua
@@ -1,17 +1,20 @@
 function dialogue(NameofSpeaker,message)
-    local x=0
+    local x=4
     local y=10
-    local w=15
+    local w=11
     local h=5
     
     text={}
     text.active=true
     str={}
-    str[1]="Hi my name is rudy barua"
-    str[2]="World"
+  --  str[1]="Hi my name is rudy barua"
+   -- str[2]="World"
 
     test=text.str
     draw_snake_box(x,y,w,h)
+    draw_snake_box(0,y,4,h)
+    
+    spr(136, 12, 96, 2, 2)
     --print(NameofSpeaker..": Hello World", 15, 50, 8)
     --write text
     print(NameofSpeaker..":",10,84)
