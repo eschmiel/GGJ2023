1c41005a74bfcc11dd4a747c44cd0df6ab6e7b79
eschmiel <eschmiel@gmail.com>
2023-03-28 20:51:06 -0400

    enemies move along the path towards the nearest player unit

--- game_scene/src/entities/enemy.lua
+++ game_scene/src/entities/enemy.lua
@@ -6,7 +6,7 @@ function setup_faction_ai(faction_manager_to_run)
             if not self:areAnyUnitsResolving() then
                 for unit in all(self.faction_manager.units) do
                     if unit.active != false then
-                        self:move_action(unit)
+                        self:move_towards_nearest_player_unit(unit)
                         break
                     end
                 end
@@ -29,7 +29,48 @@ function setup_faction_ai(faction_manager_to_run)
         end,
         
         move_towards_nearest_player_unit = function(self, unit)
+            local closestPlayerUnit = self:find_nearest_player_unit2(unit)
+            local path = dijkstra:getPath(unit.positionX, unit.positionY, closestPlayerUnit.positionX, closestPlayerUnit.positionY)
+            local all_unit_positions = faction_manager:get_all_unit_positions()
+            local targetPosition 
+            
+            if unit.movement < #path then
+                targetPosition = #path - unit.movement
+            else
+                targetPosition = 1
+            end
 
+            for position in all(all_unit_positions) do
+                local pathStep = path:findIndexOfContainedPosition(position.x, position.y)
+                if pathStep then
+                    path:remove(position.x, position.y)
+                    if pathStep < targetPosition then
+                        targetPosition -= 1
+                    end
+                end
+            end
+            if (targetPosition > 0 and #path > 0) then
+                unit:move(path[targetPosition], false)
+            else
+                unit.active = false
+            end
+        end,
+
+        find_nearest_player_unit2 = function(self, unit)
+            local player_units = faction_manager:get_all_units_based_on_faction_type(factionTypesEnum.PLAYER)
+            local enemy_unit_coordinates = unit:get_coordinate_object()
+
+            local closestPlayerUnit = player_units[1]
+            local closestPlayerUnitDistance = dijkstra:getDistanceBetweenPositions(enemy_unit_coordinates.x, enemy_unit_coordinates.y, closestPlayerUnit.positionX, closestPlayerUnit.positionY)
+            for player_unit in all(player_units) do
+                local player_unit_distance = dijkstra:getDistanceBetweenPositions(enemy_unit_coordinates.x, enemy_unit_coordinates.y, player_unit.positionX, player_unit.positionY)
+                if player_unit_distance < closestPlayerUnitDistance then
+                    closestPlayerUnit = player_unit
+                    closestPlayerUnitDistance = player_unit_distance
+                end
+            end
+
+            return closestPlayerUnit
         end,
 
         -- could probably abstract find_nearest_player_unit and find_tile_in_range_closest_to_nearest_player_unit into the same function, driven by parameters
